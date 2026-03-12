82f795350e3f7491234bd44420aa750593c898fb
Amber Spath <122047785+DeckerGirl@users.noreply.github.com>
2026-03-12 12:21:57 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,31 @@
 const express = require("express");
 const cors = require("cors");
-
+const { MongoClient, ObjectId } = require("mongodb");
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 
+// Basic routes
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-app.get("/api/test", (req, res) => {
-  res.json({ message: "Backend connected successfully" });
-});
+// MongoDB connection
+async function connectDB() {
+  const uri = "mongodb://localhost:27017/";
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    db = client.db("Restaurant");
+    console.log("Connected to MongoDB!");
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
