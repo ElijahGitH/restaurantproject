fab9e4ba8d166e82510bea7a9e6451a0ee757139
Amber Spath <122047785+DeckerGirl@users.noreply.github.com>
2026-03-16 22:19:15 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";   // add this
 import LoginPage from "./pages/LoginPage";
 import AdminDashboard from "./pages/AdminDashboard";
 import UserDashboard from "./pages/UserDashboard";
@@ -7,11 +8,15 @@ import "./App.css";
 function App() {
   return (
     <Router>
+
+      <Navbar />  {/* navbar appears on all pages */}
+
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/user" element={<UserDashboard />} />
       </Routes>
+
     </Router>
   );
 }
