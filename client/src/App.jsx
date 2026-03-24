3e21bf60d35d8392556855cc0b2fdbc1137f164d
Amber Spath <122047785+DeckerGirl@users.noreply.github.com>
2026-03-24 19:46:59 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+
 import LoginPage from "./pages/LoginPage";
 import AdminDashboard from "./pages/AdminDashboard";
 import UserDashboard from "./pages/UserDashboard";
-import "./App.css";
-
 function App() {
-  return (
-    <Router>
+    return (
+        <Router>
+            <Navbar />
 
-      {/* Navbar always visible */}
-      <Navbar />
-
-      {/* Page content changes below */}
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/user" element={<UserDashboard />} />
-      </Routes>
-
-    </Router>
-  );
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/admin" element={<AdminDashboard />} />
+                <Route path="/user" element={<UserDashboard />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
