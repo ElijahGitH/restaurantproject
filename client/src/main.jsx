c0d902bb94b184785088a24dd7439e6d824b27e4
Dman2004256 <dy183875@ucf.edu>
2026-03-25 13:41:12 -0400

    main update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,57 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import AddCustomer from './pages/AddCustomer.jsx'
+import AddAdmin from './pages/AddAdmin.jsx'
+import UserDashboard from './pages/UserDashboard.jsx'
+import AdminDashboard from './pages/AdminDashboard.jsx'
 
-createRoot(document.getElementById('root')).render(
+let root = createRoot(document.getElementById('root'));
+
+export function login(){
+  root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <LoginPage/>
+    <button onClick={()=>AddCust()}>Add Customer</button>
+    <button onClick={()=>AddAdministrator()}>Add Administrator</button>
+  </StrictMode>)
+}
+
+function AddCust(){
+  root.render(
+    <StrictMode>
+      <AddCustomer/>
+      <button onClick={()=>login()}>Return to Login</button>
+    </StrictMode>
+  )
+}
+
+function AddAdministrator(){
+  root.render(
+    <StrictMode>
+      <AddAdmin/>
+      <button onClick={()=>login()}>Return to Login</button>
+    </StrictMode>
+  )
+}
+
+export function CustomerDash(){
+  root.render(
+    <StrictMode>
+      <UserDashboard/>
+    </StrictMode>
+  )
+
+}
+
+export function AdminDash(){
+  root.render(
+    <StrictMode>
+      <AdminDashboard/>
+    </StrictMode>
+  )
+
+}
+
+login();
