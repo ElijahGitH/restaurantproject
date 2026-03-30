ecba634d5a78b6e4d242280d452ff539d1843252
Amber Spath <122047785+DeckerGirl@users.noreply.github.com>
2026-03-29 20:57:13 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
@@ -6,8 +7,8 @@ import LoginPage from "./pages/LoginPage";
 import AdminDashboard from "./pages/AdminDashboard";
 import UserDashboard from "./pages/UserDashboard";
 import Menu from "./pages/Menu";
-
-
+import AddAdmin from "./pages/AddAdmin";
+import AddCustomer from "./pages/AddCustomer";
 
 function App() {
     return (
@@ -20,6 +21,8 @@ function App() {
                 <Route path="/admin" element={<AdminDashboard />} />
                 <Route path="/user" element={<UserDashboard />} />
                 <Route path="/menu" element={<Menu />} />
+                <Route path="/userregister" element={<AddCustomer />} />
+                <Route path="/adminregister" element={<AddAdmin />} />
             </Routes>
         </Router>
     );
