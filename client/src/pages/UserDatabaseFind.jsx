b65c96cbc0f965c44fd988eadbb61482ac2ac909
Amber Spath <122047785+DeckerGirl@users.noreply.github.com>
2026-03-30 12:57:29 -0400

    Update UserDatabaseFind.jsx

--- client/src/pages/UserDatabaseFind.jsx
+++ client/src/pages/UserDatabaseFind.jsx
@@ -2,41 +2,16 @@ import { useNavigate } from 'react-router-dom';
 /*import {CustomerDash, AdminDash} from '../main'*/
 
 function loginCheck(username, password) {
-    let response, data, isAdmin, isUser, foundUser;
-    if (username == "user") {
-        if (password == "user123") {
-            return (0);
-        }
+    if (username === "user" && password === "user123") {
+        return 0; // user
     }
 
-    if (username == "admin") {
-        if (password == "admin123") {
-            return (1);
-        }
+    if (username === "admin" && password === "admin123") {
+        return 1; // admin
     }
 
-
-    /*response = await fetch('http://localhost:5000/admin');
-    data = await response.json();
-    isAdmin = data.some(item=>item.username === username);
-
-    if(isAdmin === true){
-        foundUser = data.find((user)=>user.username === username);
-        if (foundUser.password === password){
-            return(1);
-        }
-    }
-
-    response = await fetch('http://localhost:5000/customer');
-    data = await response.json();
-    isUser = data.some(item=>item.username === username);
-
-    if(isUser === true){
-        foundUser = data.find((user)=>user.username === username);
-        if (foundUser.password === password){
-            return(0);
-        }
-    }*/
+    return -1; // invalid login
 }
 
 export default loginCheck;
+
