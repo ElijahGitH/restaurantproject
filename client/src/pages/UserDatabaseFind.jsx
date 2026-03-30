c70e3138cbc13bab005dc4f34a86ae48bf927fd4
Amber Spath <122047785+DeckerGirl@users.noreply.github.com>
2026-03-30 12:58:32 -0400

    Update UserDatabaseFind.jsx

--- client/src/pages/UserDatabaseFind.jsx
+++ client/src/pages/UserDatabaseFind.jsx
@@ -13,5 +13,27 @@ function loginCheck(username, password) {
     return -1; // invalid login
 }
 
+    /*response = await fetch('http://localhost:5000/admin');
+    data = await response.json();
+    isAdmin = data.some(item=>item.username === username);
+
+    if(isAdmin === true){
+        foundUser = data.find((user)=>user.username === username);
+        if (foundUser.password === password){
+            return(1);
+        }
+    }
+
+    response = await fetch('http://localhost:5000/customer');
+    data = await response.json();
+    isUser = data.some(item=>item.username === username);
+
+    if(isUser === true){
+        foundUser = data.find((user)=>user.username === username);
+        if (foundUser.password === password){
+            return(0);
+        }
+    }*/
+}
 export default loginCheck;
 
