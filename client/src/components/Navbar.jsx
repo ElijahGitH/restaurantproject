86eb1d78832df9581c74fb68684a23a6b6edd057
Amber Spath <122047785+DeckerGirl@users.noreply.github.com>
2026-03-30 11:49:28 -0400

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ function Navbar() {
             <h2 style={styles.logo}>Restaurant App  </h2>
             <div style={styles.links}>
                 <Link to="/" style={styles.link}>Login</Link>
+                <Link to="/home" style={styles.link}>Home</Link>
                 <Link to="/admin" style={styles.link}>Admin</Link>
                 <Link to="/user" style={styles.link}>User</Link>
                 <Link to="/menu" style={styles.link}>Menu</Link>
