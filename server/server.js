520cac10e8bcbdc12cf0c1c55795d3960f14de03
Dman2004256 <dy183875@ucf.edu>
2026-03-31 18:06:23 -0400

    Added functions to add / get users from database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const User = require("./User");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.get("/", (req, res) => {
   res.send("Server is running");
@@ -17,4 +25,27 @@ app.get("/api/test", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+
+app.post("/users", async(req, res) => {
+    try{
+      const user = await User.create({
+        username: req.body.username,
+        passwordHash: req.body.password,
+        role: req.body.role,
+      })
+    }catch(e){
+      console.log(e.code);
+      throw Error();
+    }
+});
+
+app.get("/users", async (req, res) => {
+    try{
+        const user = await User.find({});
+        res.send(user);
+    }catch(e){
+        console.log(e.code);
+    }
 });
